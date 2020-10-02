07cf9219a6399b87a61447bdac02fa7ebc386d77
Ryan Porter <71415955+PorterRyan@users.noreply.github.com>
2020-10-02 08:45:21 -0700

    Added pizza menu

---
+++ pizza_menu.py
@@ -0,0 +1,42 @@
+#Pepperoni
+pepperoni_price = "$5.00"
+pepperoni_ingredients = "Mozzarella cheese, pepperoni slices"
+
+#Cheese Pizza
+cheese_ingredients = "Mozarella cheese"
+cheese_price = "$4.00"
+
+#Margherita
+margherita_ingredients = "Tomatoes, olive oil, mozzarella cheese"
+margherita_price = "$6.00"
+
+#Meatball
+meatball_ingredients = "Tomatoe sauce, mozzarella cheese, meatballs, pecorino cheese, olive oil"
+meatball_price = "$7.00"
+
+#Sicilian
+sicilian_ingredients = "Tomato sauce, mozzarella, olive oil"
+sicilian_price = "$5.00"
+
+print("Pizza Menu")
+print()
+
+print("Pepperoni Pizza - " + pepperoni_price)
+print(pepperoni_ingredients)
+print()
+
+print("Cheese Pizza - " + cheese_price)
+print(cheese_ingredients)
+print()
+
+print("Margherita Pizza - " + margherita_price)
+print(margherita_ingredients)
+print()
+
+print("Meatball Pizza - " + meatball_price)
+print(meatball_ingredients)
+print()
+
+print("Sicilian Pizza - " + sicilian_price)
+print(sicilian_ingredients)
+print()
