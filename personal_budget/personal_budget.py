9e6ddc79417728c3e9c4081cea7624f5c9493bc1
Ryan Porter <71415955+PorterRyan@users.noreply.github.com>
2020-10-03 08:48:08 -0700

    Create personal_budget.py

---
+++ personal_budget/personal_budget.py
@@ -0,0 +1,66 @@
+# personal_budget.py
+print()
+print("Monthly Budget")
+print("=================================")
+print("Item         Month       Year")
+print("=================================")
+
+# Category 1
+cat1 = 'Gas'
+
+cat1_yearly = 600
+
+cat1_monthly = cat1_yearly / 12
+
+print("{0:<12} ${1:<10.2f} ${2:<10.2f}".format(cat1, cat1_monthly, cat1_yearly))
+print()
+
+# Category 2
+cat2 = 'Car Payment'
+
+cat2_yearly = 2400
+
+cat2_monthly = cat2_yearly / 12
+
+print("{0:<12} ${1:<10.2f} ${2:<10.2f}".format(cat2, cat2_monthly, cat2_yearly))
+print()
+
+# Category 3
+cat3 = 'Food'
+
+cat3_yearly = 3650
+
+cat3_monthly = cat3_yearly / 12
+
+print("{0:<12} ${1:<10.2f} ${2:<10.2f}".format(cat3, cat3_monthly, cat3_yearly))
+print()
+
+# Category 4
+cat4 = 'Spotify'
+
+cat4_yearly = 59.88
+
+cat4_monthly = cat4_yearly / 12
+
+print("{0:<12} ${1:<10.2f} ${2:<10.2f}".format(cat4, cat4_monthly, cat4_yearly))
+print()
+
+# Category 5
+cat5 = 'AWS'
+
+cat5_yearly = 120
+
+cat5_monthly = cat5_yearly / 12
+
+print("{0:<12} ${1:<10.2f} ${2:<10.2f}".format(cat5, cat5_monthly, cat5_yearly))
+print()
+
+# Category 6
+cat6 = 'DisneyPlus'
+
+cat6_yearly = 83.88
+
+cat6_monthly = cat6_yearly / 12
+
+print("{0:<12} ${1:<10.2f} ${2:<10.2f}".format(cat6, cat6_monthly, cat6_yearly))
+print()
